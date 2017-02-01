1b83dd474d800b99bc6e3c8dda3412c225d29ab4
raivk <thomas.lbrn76@gmail.com>
2017-02-01 14:34:04 +0100

    Remove base quintus

--- public/src/game.js
+++ public/src/game.js
@@ -1,31 +1,3 @@
-
-
-//Initialize the game with Quintus
-var Q = Quintus({audioSupported: [ 'wav','mp3' ]})
-      .include('Sprites, Scenes, Input, 2D, Anim, Touch, UI, Audio')
-      .setup({ maximize: true })
-      .enableSound();
-
-//Setup controls : both azerty + qwerty + arrows
-Q.input.keyboardControls({
-                           90: "up",
-                           87: "up",
-                           81: "left",
-                           65: "left",
-                           83: "down",
-                           68: "right",
-                           UP: "up",
-                           LEFT: "left",
-                           RIGHT: "right",
-                           DOWN: "down",
-                           32: "fire"
-                         });
-//Enable mouse controls
-Q.input.mouseControls({cursor:true});
-
-//No gravity, topdown game
-Q.gravityY = 0;
-
 //We need socket.io to enable connection with server
 require(['socket.io/socket.io.js']);
 
