134c6ba5b9cd6fa9203100d26b88f1e74e4e2651
raivk <thomas.lbrn76@gmail.com>
2017-02-01 14:27:32 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,18 @@
+# io_game
+Project of a simple multiplayer web browser game
+
+Installation :
+
+Install node.js
+clone repository
+in root of project:
+run the following commands :
+npm install --save express
+npm install --save socket.io
+
+Run :
+
+In project root, where there is app.js file, do the following:
+run a terminal
+run the command : node app.js
+Go to localhost:8080 and play. You can unode app.jsse multiple tabs to test multiplayer (BUG IN CHROME : need to use a different window for each tab in order for multiplayer to work).
---
+++ app.js
@@ -0,0 +1,34 @@
+//Setup base variables for the server to work
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+//We'll use the public folder, so we tell the server to place public in front of each path (avoid repetitions)
+app.use(express.static(__dirname + '/public'));
+
+//We use a simple one page app, so tell the server to render the index
+app.get('/', function(req, res){
+  res.render('/index.html');
+});
+
+//Code executed on each new connection
+io.on('connection', function (socket) {
+    console.log("Connection");
+    
+    //Event received after player clicked ready
+    socket.on('ready', function(data){
+        console.log("ready " + data.exemple );
+        socket.broadcast.emit("ready_other", data);
+    });
+    
+    //Someone disconnected
+    socket.on('disconnect', function(){
+        console.log("Disconnect");
+    });
+});
+
+//port = process.env.PORT
+port = 8080
+server.listen(port);
+console.log("Multiplayer app listening on port "+port);
