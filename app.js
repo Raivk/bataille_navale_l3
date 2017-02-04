7af8db735349fe52f336190a57e6a4827eb63794
Raivk <thomas.lbrn76@gmail.com>
2017-02-04 16:37:09 +0100

    Implementation basique du systemes de pages, et interactions serveurs pour le changement d'etat

--- app.js
+++ app.js
@@ -18,13 +18,102 @@ app.get('/', function(req, res){
 io.on('connection', function (socket) {
     console.log("Connection");
     
-    //Event received after player clicked ready
-    socket.on('ready', function(data){
-        console.log("ready " + data.exemple );
-        socket.broadcast.emit("ready_other", data);
+    //MMR-------------------------------
+    
+    socket.on('mmr_search', function(){
+        console.log("demarrage d'une recherche");
+        //RECHERCHER SALON
+        
+        //TROUVE
+        console.log("partie trouvee");
+        socket.emit('found');
+        
+        socket.on('cancel_search',function(data){
+            if(data.sal_state){
+                //SUPPRESION DU SALON
+                console.log("cancel recherche adversaire");
+            }
+            else{
+                //ARRET DE LA RECHERCHE
+                console.log("cancel recherche salon");                
+            }
+        });
+        
+        //PAS TROUVE
+        //OUVRIR UN SALON PUBLIC
+        //console.log("partie pas trouvee, ouverture de salon public");
+        //socket.emit('not_found');
     });
     
-    //Someone disconnected
+    //SALON PRIVE-------------------------------
+    
+    socket.on('private_init', function(){
+        //CALCULER UNE CLE
+        console.log("ouverture d'un salon prive");
+        var key = 0123;
+        socket.emit('key_code',{kc:key});
+        
+        socket.on('cancel_private',function(){
+            //SUPPRESSION DU SALON PRIVE
+            console.log("cancel private");
+        })
+        
+        //RIVAL TROUVE
+        //socket.emit('found_rival');
+    });
+    
+    //RECHERCHE SALON PRIVE-------------------------------
+    
+    socket.on("private_search", function(data){
+        //attribut en question : data.sal_key
+        console.log("recherche de salon pour la clé "+data.sal_key);
+        var temporary = 0123;
+        if(data.sal_key == temporary){
+            //CHANGER LE TEST POUR UN PARCOURS DES CLES
+            //ICI C'EST QUAND ON A TROUVE LA CLE QUI CORRESPOND
+            //TESTER SI IL Y A DE LA PLACE DANS LE SALON
+            console.log("cle valide, preparation de partie");
+            socket.emit("key_response",{found:true});
+        }
+        else{
+            //PAS VALIDE
+            console.log("clee invalide");
+            socket.emit("key_response",{found:false});
+        }
+    })
+    
+    //READY-------------------------------
+    
+    socket.on("ready",function(){
+        //PREVENIR L'ADVERSAIRE
+        //SI LES DEUX SON PRET
+        console.log("quelqu'un est pret");
+        var both_ready = true;
+        if(both_ready){
+            console.log("on demarre la partie");
+            socket.emit("go_party");
+            //PREVENIR L'AUTRE : DEMMARER LA PARTIE
+        }
+        else{
+            console.log("l'autre joueur n'est pas pret")
+            //l'autre est pas prêt, attendre sa réponse
+        }
+    });
+    
+    socket.on("cancel_ready",function(){
+        //PREVENIR L'AUTRE QUE C'EST FOUTU
+        //evenement "other_cancel" à envoyer à l'autre joueur
+        console.log("annulation de partie au menu pret");
+    });
+    
+    //INGAME-------------------------------
+    
+    socket.on("quit_game",function(){
+        //PREVENIR L'AUTRE QUE SON ADVERSAIRE A QUITTE LA PARTIE
+        console.log("partie quitee");
+    })
+    
+    //DISCONNECT-------------------------------
     socket.on('disconnect', function(){
         console.log("Disconnect");
     });
--- public/src/game.js
+++ public/src/game.js
@@ -4,15 +4,112 @@ require(['socket.io/socket.io.js']);
 
 var socket = io.connect('localhost:8080');
 
+var salon = false;
 
-function ready(){
-    socket.emit('ready',{exemple:"myname"});
+//OUTILS-------------------------------------
+
+function switch_page(tohide, togo){
+    document.getElementById(tohide).style.display = "none";
+    document.getElementById(togo).style.display = "block";
 }
 
-socket.on('connected', function (data) {
-    console.log("Connected");
-});
+//MMR-------------------------------------
 
-socket.on('ready_other', function(data){
-    console.log("Someone is ready " + data.exemple); 
-});
+function annuler_recherche(){
+    socket.emit('cancel_search',{sal_state:salon});
+    salon = false;
+    switch_page("mmr_search","home");
+}
+
+function demarrer_mmr(){
+    switch_page("home","mmr_search");
+    switch_page("attente","rech");
+    
+    socket.emit("mmr_search");
+    
+    socket.on('not_found', function(){
+        //OUVERTURE D'UN SALON
+        salon = true;
+        switch_page("rech","attente");
+    });
+    
+    socket.on('found', function(){
+        switch_page("mmr_search", "ready");
+        menu_pret();
+        //FAIRE QUELQUE CHOSE ?
+        //PARTIE TROUVEE
+    });
+}
+
+//SALON PRIVE-------------------------------------
+
+function annuler_salon_prive(){
+    //PREVENIR LE SERVEUR DE LA FERMETURE DU SALON PRIVE
+    socket.emit("cancel_private");
+    switch_page("private_room_waiting","home");
+}
+
+function demarrer_salon(){
+    socket.emit('private_init');
+    socket.on('key_code', function(data){
+        document.getElementById("key").innerHTML = data.kc;
+        switch_page("home","private_room_waiting");
+        //ATTENTE DE JOUEUR
+        socket.on('found_rival', function(){
+            switch_page("private_room_waiting","ready");
+            menu_pret();
+        });
+    });
+}
+
+
+//RECHERCHE SALON PRIVE-------------------------------------
+
+function rejoindre_prive(){
+    //RECUPERER LES DONNEES DU CHAMPS DE CLE
+    socket.emit("private_search", {sal_key : 0123});
+    socket.on("key_response", function(data){
+        if(data.found){
+            //PARTIE TROUVEE, FAIRE QUELQUE CHOSE
+            switch_page("home","ready");
+            menu_pret();
+        }
+        else{
+            console.log("clée invalide");
+        }
+    });
+}
+
+//READY-------------------------------------
+
+function menu_pret(){
+    socket.on("other_ready",function(){
+        console.log("l'autre est prêt");
+    });
+    socket.on("other_cancel",function(){
+        console.log("l'autre annule la partie au menu pret");
+        switch_page("ready","home");
+    })
+}
+
+function annuler_pret(){
+    socket.emit("cancel_ready");
+    switch_page("ready","home");
+}
+
+function declarer_pret(){
+    console.log("PRET !");
+    //AFFICHER QUELQUE CHOSE A L'ECRAN DU JOUEUR
+    socket.emit("ready");
+    //ATTENDRE LA REPONSE DU SERVEUR, si l'autre est pret, go en jeu
+    socket.on("go_party",function(){
+       switch_page("ready","ingame"); 
+    });
+}
+
+//INGAME-------------------------------------
+
+function quitter_partie(){
+    socket.emit("quit_game");
+    switch_page("ingame","home");
+}
